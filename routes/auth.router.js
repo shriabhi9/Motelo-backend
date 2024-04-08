cb7ad3bd9a978456c5f132cc1084fcb69773dd45
Abhishek Shrivastava <shriabhishek929@gmail.com>
2024-04-08 22:35:38 +0530

    Travelo

---
+++ routes/auth.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router =  express.Router();
+
+router.route("/register").post(async (req,res)=>{
+    try {
+        const newUser = x;
+    } catch (error) {
+        console.log(error)
+    }
+})
