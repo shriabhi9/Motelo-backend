6c267cb7495261c08a4a724aae499602ac89397c
abhishek <shriabhishek929@gmail,com>
2024-04-30 09:39:10 -0700

    Added timestamps

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,11 @@ const userSchema = new mongoose.Schema({
     number:{type:Number,required:true,uniquie:true},
     email:{type:String,required:true,uniquie:true},
     password:{type:String,required:true},
-})
+},
+{
+    timestamps:true
+}
+)
 
 const User = mongoose.model("User",userSchema)
 module.exports = User;
