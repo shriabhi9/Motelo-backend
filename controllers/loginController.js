19f851b1dd3ff89772b55937f6b95c3ecd21c1be
abhishek <shriabhishek929@gmail.com>
2024-07-04 01:25:12 +0530

    minor changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,7 +2,7 @@ const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 const loginHandler = async(req,res)=>{
     try {
-        const user = await User.findOne({number: req.body.number})
+        const user = await user.findOne({number: req.body.number})
         !user && res.status(401).json({message:"Invalid Mobile Number"})
 
         const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(CryptoJS.enc.Utf8);
