c9e988eb992181cc23d529de812a04540f61090e
abhishek <shriabhishek929@gmail.com>
2024-07-05 10:40:22 +0530

    login api fixed

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,24 +1,37 @@
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const loginHandler = async(req,res)=>{
-    try {
-        const user = await user.findOne({number: req.body.number})
-        !user && res.status(401).json({message:"Invalid Mobile Number"})
+const User = require("../models/user.model");
+const loginHandler = async (req, res) => {
+  try {
+    const user = await User.findOne({ number: req.body.number });
 
-        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(CryptoJS.enc.Utf8);
-        decodedPassword !== req.body.password && res.status(401).json({
-            message:"Incorrect Password"
-        })
-
-        const {password, ...rest} = user._doc;
-        const accessToken = jwt.sign({username:user.username},process.env.ACCESS_TOKEN)
-
-        res.json(...rest,accessToken);
-    } catch (error) {
-        res.status(404).json({
-            message:"Something wrong page not " 
-        })
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Mobile Number" });
     }
-}
 
+    const decodedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (decodedPassword !== req.body.password) {
+      return res.status(401).json({ message: "Incorrect Password" });
+    }
+
+    const { password, ...rest } = user._doc;
+    const accessToken = jwt.sign(
+      { username: user.username },
+      process.env.ACCESS_TOKEN
+    );
+
+    console.log("Login successful for user:", user.username);
+
+    return res.json({ ...rest, accessToken });
+  } catch (error) {
+    console.error("Error during login:", error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Please try again later." });
+  }
+};
 module.exports = loginHandler;
